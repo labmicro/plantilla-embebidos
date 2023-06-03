80040e0820fadd671a96d38f4e06dc1f24c8fad3
Joel Jassan <joeljassan@hotmail.com>
2023-06-03 15:55:26 -0300

    Se cambian las definiciones de pines para soporte del poncho. Se crean sus respectivas funciones.

--- inc/bsp.h
+++ inc/bsp.h
@@ -20,19 +20,14 @@
 
 /*---  Public Data Declaration  ---------------------------------------------------------------- */
 
-//! Estructura que almacena los descriptores de la bsp
 typedef struct board_s {
-    digital_output_t led_RGB_rojo;  //!< Denotado R
-    digital_output_t led_RGB_verde; //!< Denotado G
-    digital_output_t led_RGB_azul;  //!< Denotado B
-    digital_output_t led_amarillo;  //!< Denotado LED 1 en placa
-    digital_output_t led_rojo;      //!< Denotado LED 2 en placa
-    digital_output_t led_verde;     //!< Denotado LED 3 en placa
-
-    digital_input_t tec_1;          //!< Denotado TEC 1 en placa
-    digital_input_t tec_2;          //!< Denotado TEC 2 en placa
-    digital_input_t tec_3;          //!< Denotado TEC 3 en placa
-    digital_input_t tec_4;          //!< Denotado TEC 4 en placa
+    digital_output_t buzzer;
+    digital_input_t set_time;
+    digital_input_t set_alarm;
+    digital_input_t decrement;
+    digital_input_t increment;
+    digital_input_t accept;
+    digital_input_t cancel;
 } const * const board_t;
 
 /*---  Public Function Declaration  ------------------------------------------------------------ */
--- src/bsp.c
+++ src/bsp.c
@@ -29,6 +29,14 @@ static struct board_s board = {0};
 
 /*---  Private Function Declaration  ----------------------------------------------------------- */
 
+//!Funcion de configuracion del buzzer
+void BuzzerInit(void);
+
+//!Funcion de configuracion de las teclas
+void KeysInit(void);
+
+
+
 /*---  Public Function Declaration  ------------------------------------------------------------ */
 
 /*---  Private Data Definition  ---------------------------------------------------------------- */
@@ -41,40 +49,23 @@ static struct board_s board = {0};
 
 /*---  Private Function Implementation  -------------------------------------------------------- */
 
-board_t BoardCreate(void) {
-    Chip_SCU_PinMuxSet(LED_R_PORT, LED_R_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_R_FUNC);
-    board.led_RGB_rojo = DigitalOutputCreate(LED_R_GPIO,LED_R_BIT, inverted_logic);
-
-    Chip_SCU_PinMuxSet(LED_G_PORT, LED_G_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_G_FUNC);
-    board.led_RGB_verde = DigitalOutputCreate(LED_G_GPIO,LED_G_BIT, inverted_logic);
-
+void BuzzerInit(void){
     Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
-    board.led_RGB_azul = DigitalOutputCreate(LED_B_GPIO,LED_B_BIT, inverted_logic);
+    board.buzzer = DigitalOutputCreate(LED_B_GPIO,LED_B_BIT, inverted_logic);
+}
 
-    /******************/
-    Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
-    board.led_rojo = DigitalOutputCreate(LED_1_GPIO,LED_1_BIT, inverted_logic);
-
-    Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
-    board.led_amarillo = DigitalOutputCreate(LED_2_GPIO,LED_2_BIT, inverted_logic);
-
-    Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
-    board.led_verde = DigitalOutputCreate(LED_3_GPIO, LED_3_BIT, inverted_logic);
-
-    /******************/
+void KeysInit(void){
     Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
-    board.tec_1 = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, inverted_logic);
+    board.accept = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, true);
 
     Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
-    board.tec_2 = DigitalInputCreate(TEC_2_GPIO, TEC_2_BIT, inverted_logic);
+    board.cancel = DigitalInputCreate(TEC_2_GPIO, TEC_2_BIT, true);
 
     Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
-    board.tec_3 = DigitalInputCreate(TEC_3_GPIO, TEC_3_BIT, inverted_logic);
+    board.set_time = DigitalInputCreate(TEC_3_GPIO, TEC_3_BIT, true);
 
     Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
-    board.tec_4 = DigitalInputCreate(TEC_4_GPIO, TEC_4_BIT, inverted_logic);
-
-    return &board;
+    board.set_alarm = DigitalInputCreate(TEC_4_GPIO, TEC_4_BIT, true);
 }
 
 /*---  Public Function Implementation  --------------------------------------------------------- */
