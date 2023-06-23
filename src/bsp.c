d4392352123fdfe6600358638a854fd6007fadf5
Joel Jassan <joeljassan@hotmail.com>
2023-06-23 00:59:27 -0300

    Se acomodan los digitos

--- src/bsp.c
+++ src/bsp.c
@@ -196,7 +196,7 @@ void SegmentsTurnOn(uint8_t segments) {
 }
 
 void DigitTurnOn(uint8_t digit) {
-	Chip_GPIO_SetValue(LPC_GPIO_PORT, DIGITS_GPIO, ((1 << (digit)) & DIGITS_MASK));
+	Chip_GPIO_SetValue(LPC_GPIO_PORT, DIGITS_GPIO, ((1 << (3 - digit)) & DIGITS_MASK));
 }
 
 /*---  Public Function Implementation  --------------------------------------------------------- */
